05c94dd3e1ecafd7660bbf44c1af3d65e0b32f3a
tanuj <tanujvyas10@gmail.com>
2020-08-12 02:06:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <div className = "game-container">
+    <Figure/>  
+    <WrongLetters/>
+    <Word/>
+    <Popup/>
+    <Notification/>
+    </div>
+   
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <h1>Handman</h1> 
+            <p>Find the hidden word</p>
+        </>
+    )
+}
+
+export default Header
